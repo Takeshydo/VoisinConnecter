b29a4b074a3627bbd2b42df57e4bb84d2752cf68
MathysL <m.lacoque@ecole-ippsi.net>
2026-04-01 19:52:18 +0200

    enlever un S a annonce

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class AnnoncesController extends AbstractController
+final class AnnonceController extends AbstractController
 {
     public function __construct(
         private EntityManagerInterface $em,
